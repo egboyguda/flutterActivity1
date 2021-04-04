821c86028b5caf247e0fdffef5a7d9e1026d64a2
egboyguda <e.gboyguda@gmail.com>
2021-04-04 23:04:12 +0800

    2nd commit

--- layout_challenge/lib/main.dart
+++ layout_challenge/lib/main.dart
@@ -11,17 +11,61 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Row(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
-                )
-              ],
-            )
-          ],
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      width: 100.0,
+                      color: Colors.red,
+                      child: Text('asda'),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.yellow,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.green,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      width: 100.0,
+                      color: Colors.blue,
+                      child: Text('asda'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
