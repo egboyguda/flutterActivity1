7d2e5fc5b92e546c8dce8aa3c0de37de252cb499
egboyguda <e.gboyguda@gmail.com>
2021-04-04 22:03:30 +0800

    first commit

---
+++ layout_challenge/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  color: Colors.red,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
